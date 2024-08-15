1acca6886cc9abced57143a96a46b887d729677d
raymondnajem <78000746+raymondnajem@users.noreply.github.com>
2024-08-14 21:30:16 -0400

    Clean up

--- src/main/java/well/demo/strings/StringDetectorController.java
+++ src/main/java/well/demo/strings/StringDetectorController.java
@@ -1,44 +1,31 @@
 package well.demo.strings;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
-
-
 @RestController
 @RequestMapping("/api")
 public class StringDetectorController {
 
-	
 	@Autowired
-	private StringDetectorService stringDectorService;
-	
+	private StringDetectorService stringDetectorService;
+
 	/**
 	 * GET implementation
 	 */
-	@RequestMapping(method=RequestMethod.GET, value="/query")
-	public int hasKey(@RequestParam(name="key", required=true) String key) {
-		 return  stringDectorService.hasKey(key);
-		 
-	 }
-	
-	@RequestMapping(method=RequestMethod.POST, value="/input", consumes = "text/plain")
+	@RequestMapping(method = RequestMethod.GET, value = "/query")
+	public String hasKey(@RequestParam(name = "key", required = true) String key) {
+		return stringDetectorService.hasKey(key);
+
+	}
+
+	@RequestMapping(method = RequestMethod.POST, value = "/input", consumes = "text/plain")
 	public void addString(@RequestBody String topic) {
-		stringDectorService.addString(topic);
+		stringDetectorService.addString(topic);
 	}
-	
-	
-	@RequestMapping("/hello")
-	public String sayHi() {
-		return "Hello";
-	}
-	
-	
-	
-	
+
 }
--- src/main/java/well/demo/strings/StringDetectorService.java
+++ src/main/java/well/demo/strings/StringDetectorService.java
@@ -2,55 +2,46 @@ package well.demo.strings;
 
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
+import java.util.concurrent.atomic.AtomicInteger;
 
 import org.springframework.stereotype.Service;
 
 @Service
 public class StringDetectorService {
-	
-	private static Map<String, Integer> sd = new ConcurrentHashMap<String, Integer>();
-	
+
+	private final static Map<String, Integer> stringCollection = new ConcurrentHashMap<String, Integer>();
+
 	/**
-	 * If a String is not in the map, it is added to the map. If it is already in the map, it is just increased by 1.
-	 * This maps to the POST Request
+	 * If a String is not in the map, it is added to the map. If it is already in
+	 * the map, it is just increased by 1. This maps to the POST Request
+	 * 
 	 * @param newline
 	 */
 	public void addString(String newline) {
-		if (sd.containsKey(newline)) {
-			int j = sd.get(newline);
+		if (stringCollection.containsKey(newline)) {
+			int j = stringCollection.get(newline);
 			int NewValue = j + 1;
-			sd.replace(newline, NewValue);
-		
+			stringCollection.replace(newline, NewValue);
+
 		} else {
-			sd.put(newline, 1);
+			stringCollection.put(newline, 1);
 		}
+	}
+
+	/*
+	 * Check to see if the String is in the collection. If it is it returns the
+	 * number of times it was seen by the server. If it is not it returns 0 THis
+	 * maps to the GET Request
+	 */
+	public String hasKey(String key) {
+		int keyfreq = 0;
+		if (stringCollection.containsKey(key)) {
+			keyfreq = stringCollection.get(key);
+		} else {
+			keyfreq = 0;
+		}
+		return new String(Integer.toString(keyfreq));
 
 	}
-	
-	public void tempvalue() {
-		this.addString("HELLOWORLD");
-	}
-	
-	/*
-	 * Check to see if the String is in the collection. 
-	 * If it is it returns the number of times it was seen by the server. If it is not it returns 0
-	 * THis maps to the GET Request
-	 */
-	public int hasKey(String key) {
-		this.tempvalue();
-		int keyfreq=0;
-		if (sd.containsKey(key)) {
-			keyfreq=sd.get(key);
-		}
-		else {
-			keyfreq=0;
-		}
-		return keyfreq;
-		
-		
-	}
-	
-	
-	
 
 }
